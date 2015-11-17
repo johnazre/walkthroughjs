1fea90fc5458017502cac4b4343cf0416f1331bf
johnazre <jaboyak@gmail.com>
2015-11-17 14:34:30 -0600

    text validation done for step 1

--- js/stepsCtrl.js
+++ js/stepsCtrl.js
@@ -1,7 +1,16 @@
 angular.module('WalkthroughJS').controller('stepsCtrl', function ($scope) {
 
-  $scope.editor = ace.edit("editor");
-  editor.setTheme("ace/theme/monokai");
-  editor.getSession().setMode("ace/mode/javascript");
+  $scope.correctStuff = {
+    'var_ex1': false,
+    'var_ex2': false,
+    'var_ex3': false,
+  };
+
+  $scope.checkVarAnswers = function () {
+    if($scope.var_ex1_var === 'dozen' && $scope.var_ex1_val === '12') {$scope.correctStuff.var_ex1 = true;}
+    if($scope.var_ex2_var === 'cokeOrPepsi' && ($scope.var_ex2_val.toLowerCase() === 'coke' || $scope.var_ex2_val.toLowerCase() === 'pepsi')) {$scope.correctStuff.var_ex2 = true;}
+    if($scope.var_ex3_var === 'javascriptIsAwesome' && ($scope.var_ex3_val === 'true' || $scope.var_ex3_val === 'false')) {$scope.correctStuff.var_ex3 = true;}
+  };
+
 
 });
