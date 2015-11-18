ab22e156502fa669b4986b359e4a17a9a9a631b6
johnazre <jaboyak@gmail.com>
2015-11-17 21:10:50 -0600

    adding pages

---
+++ js/stepsCtrl.js
@@ -0,0 +1,35 @@
+angular.module('WalkthroughJS').controller('stepsCtrl', function ($scope) {
+
+  $scope.correctStuff = {
+    'var_ex1': false,
+    'var_ex2': false,
+    'var_ex3': false,
+    'func_ex1': false,
+    'func_ex2': false,
+    'cond_ex1': false,
+    'cond_ex2': false,
+    'cond_ex3': false,
+    'loops_ex1': false,
+    'loops_ex2': false,
+  };
+
+  $scope.checkVarAnswers = function () {
+    if($scope.var_ex1_var === 'dozen' && $scope.var_ex1_val === '12') {$scope.correctStuff.var_ex1 = true;}
+    if($scope.var_ex2_var === 'cokeOrPepsi' && ($scope.var_ex2_val.toLowerCase() === 'coke' || $scope.var_ex2_val.toLowerCase() === 'pepsi')) {$scope.correctStuff.var_ex2 = true;}
+    if($scope.var_ex3_var === 'javascriptIsAwesome' && ($scope.var_ex3_val === 'true' || $scope.var_ex3_val === 'false')) {$scope.correctStuff.var_ex3 = true;}
+  };
+
+  $scope.checkFuncAnswers = function () {
+    alert('works');
+
+    if($scope.func_ex1_name === "sumOfTwo" && ($scope.func_ex1_arg1===$scope.func_ex1_arg3  && $scope.func_ex1_arg2===$scope.func_ex1_arg4)){ $scope.correctStuff.func_ex1 = true; }
+    if($scope.func_ex2_name === "helloThere" && ($scope.func_ex2_arg1===$scope.func_ex2_arg2)){ $scope.correctStuff.func_ex2 = true; }
+  };
+
+  $scope.checkCondAnswers = function () {
+    if($scope.cond_ex1_var1 === 'x' && $scope.cond_ex1_val1 === '5') { $scope.correctStuff.cond_ex1 = true; }
+    if($scope.cond_ex1_var2 === 'y' && $scope.cond_ex1_val2 === '10') { $scope.correctStuff.cond_ex2 = true; }
+    if($scope.cond_ex2_var1 === 'y' && $scope.cond_ex2_var2 === 'x') { $scope.correctStuff.cond_ex3 = true; }
+  };
+
+});
