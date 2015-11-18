f2483c8c5b6d0902b3cbeee6aecd2b6432c54598
johnazre <jaboyak@gmail.com>
2015-11-17 23:34:38 -0600

    second commit

--- js/stepsCtrl.js
+++ js/stepsCtrl.js
@@ -11,6 +11,12 @@ angular.module('WalkthroughJS').controller('stepsCtrl', function ($scope) {
     'cond_ex3': false,
     'loops_ex1': false,
     'loops_ex2': false,
+    'arr_ex1': false,
+    'arr_ex2': false,
+    'arr_ex3': false,
+    'obj_ex1': false,
+    'obj_ex2': false,
+    'obj_ex3': false,
   };
 
   $scope.checkVarAnswers = function () {
@@ -20,8 +26,6 @@ angular.module('WalkthroughJS').controller('stepsCtrl', function ($scope) {
   };
 
   $scope.checkFuncAnswers = function () {
-    alert('works');
-
     if($scope.func_ex1_name === "sumOfTwo" && ($scope.func_ex1_arg1===$scope.func_ex1_arg3  && $scope.func_ex1_arg2===$scope.func_ex1_arg4)){ $scope.correctStuff.func_ex1 = true; }
     if($scope.func_ex2_name === "helloThere" && ($scope.func_ex2_arg1===$scope.func_ex2_arg2)){ $scope.correctStuff.func_ex2 = true; }
   };
@@ -31,5 +35,22 @@ angular.module('WalkthroughJS').controller('stepsCtrl', function ($scope) {
     if($scope.cond_ex1_var2 === 'y' && $scope.cond_ex1_val2 === '10') { $scope.correctStuff.cond_ex2 = true; }
     if($scope.cond_ex2_var1 === 'y' && $scope.cond_ex2_var2 === 'x') { $scope.correctStuff.cond_ex3 = true; }
   };
+
+  $scope.checkLoopAnswers = function () {
+    if($scope.loops_ex1_var === 'i' && $scope.loops_ex1_val === '1') { $scope.correctStuff.loops_ex1 = true; }
+    if($scope.loops_ex2_var1 === 'i' && $scope.loops_ex2_val === '6') { $scope.correctStuff.loops_ex2 = true;}
+  }
+
+  $scope.checkArrayAnswers = function () {
+    if($scope.arr_ex1_var === 'emptyArray' && $scope.arr_ex1_val) { $scope.correctStuff.arr_ex1 = true; }
+    if($scope.arr_ex2_var === 'emptyArray') { $scope.correctStuff.arr_ex2 = true; }
+    if($scope.arr_ex3_var === 'emptyArray' && $scope.arr_ex3_val === '0') { $scope.correctStuff.arr_ex3 = true; }
+  }
+
+  $scope.checkObjAnswers = function () {
+    if($scope.obj_ex1_var === 'emptyObj' && $scope.obj_ex1_val === '{}') { $scope.correctStuff.obj_ex1 = true;}
+    if($scope.obj_ex2_var === 'emptyObj' && $scope.obj_ex2_key === "firstName" && $scope.obj_ex2_val.toLowerCase() === "james") { $scope.obj_ex2 = true;}
+    if($scope.obj_ex3_var === 'emptyObj' && $scope.obj_ex3_key === 'lastName' && $scope.obj_ex3_val.toLowerCase() === 'jameson') { $scope.obj_ex3 = true;}
+  }
 
 });
