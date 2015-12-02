659995f7e7b2ac975f86e580ab9c4e7440a16205
johnazre <jaboyak@gmail.com>
2015-12-02 08:36:59 -0600

    another commit

--- js/directives/codeBlock.js
+++ js/directives/codeBlock.js
@@ -2,7 +2,7 @@ angular.module('WalkthroughJS').directive('codeBlock', function () {
   return {
     restrict: 'E',
     transclude: true,
-    // templateUrl: "templates/codeBlock.html",
+    templateUrl: "templates/codeBlock.html",
     link: function(scope, element, attrs, ctrl, transclude) {
       transclude(scope.$parent, function(clone, scope) {
         element.append(clone);
