c25443f95b3a989e09c98b988efd002b48eaaea6
johnazre <jaboyak@gmail.com>
2016-01-23 20:40:26 -0600

    fixed bad directives

--- js/directives/stepThreeOverview.js
+++ js/directives/stepThreeOverview.js
@@ -2,6 +2,13 @@ angular.module('WalkthroughJS').directive('stepThreeOverview', function () {
 
   return {
     restrict: 'E',
-    templateUrl: 'templates/stepThreeOverview.html'
+    templateUrl: 'templates/stepThreeOverview.html',
+    transclude: true,
+    link: function(scope, element, attrs, ctrl, transclude) {
+
+      transclude(scope.$parent, function(clone, scope) {
+        element.append(clone);
+      });
+    },
   }
 });
